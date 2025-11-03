99aec0ba431de2ffe4a75373a13a0d7ac9bb4fb9
JenishAgravat <jenishagravat04@gmail.com>
2025-11-03 22:32:39 +0530

    crud operation

--- src/app.js
+++ src/app.js
@@ -1,30 +1,73 @@
-const express=require("express");
-const connectdb= require("./config/database");
-const User=require("./models/user");
-const app=express();
+const express = require("express");
+const connectdb = require("./config/database");
+const User = require("./models/user");
+const app = express();
 
-app.post("/signUp", async(req,res)=>{
-        const user=new User({
-                firstName:"jenish",
-                lastName:"agravat",
-                emaiId:"jenish@gmail.com",
-                password:"jenish1234"
-        });
+app.use(express.json());
+
+app.post("/signUp", async (req, res) => {
+  const user = new User(req.body);
+  try {
+    await user.save();
+    res.send("user inserted successfully...");
+  } catch (err) {
+    res.status(400).send("error saving user:" + err.message);
+  }
+});
+
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+  try {
+    const users = await User.find({ emaiId: userEmail });
+    if (users.length === 0) {
+      res.status(404).send("user not found");
+    } else {
+      res.send(users);
+    }
+  } catch (err) {
+    res.status(400).send("something webt wrong");
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("something went wrong");
+  }
+});
+
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+ //const user = await User.findByIdAndDelete(_id:userId);
+    const user = await User.findByIdAndDelete(userId);
+    res.send("user deleted successfully");
+  } catch (err) {
+    res.status(400).send("something went wrong");
+  }
+});
+
+app.patch("/user", async(req,res)=>{
+        const userId=req.body.userId;
+        const data=req.body;
+        console.log(data);
         try{
-        await user.save();
-        res.send("user inserted successfully...")
-        }catch(err){
-                res.status(400).send("error saving user:"+err.message)
-        }
+                await User.findByIdAndUpdate(userId,data);
+                res.send("user updated successfully");
+        }catch (err) {
+    res.status(400).send("something went wrong");
+  }
 });
 
-connectdb().then(()=>{
-    console.log("connected successfully...");   
-    app.listen(1111,()=>{
-   console.log("server created successfully...");
-});
-})
-.catch((err)=>{
-    console.log("not connected!!!")
-})
-
+connectdb()
+  .then(() => {
+    console.log("connected successfully...");
+    app.listen(1111, () => {
+      console.log("server created successfully...");
+    });
+  })
+  .catch((err) => {
+    console.log("not connected!!!");
+  });
